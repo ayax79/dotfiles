326fd8fc614063bbd0f5335bfcf616ba331a3783
Jack Wright <jack.wright@disqo.com>
2023-11-29 18:57:50 -0800

    works arounds for https://github.com/nix-community/home-manager/issues/3100

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -5,5 +5,10 @@
       enable = true;
       plugins = ["git" "vi-mode"];
     };
+    envExtra = ''
+      # hack to deal with https://github.com/nix-community/home-manager/issues/3100
+      unset __HM_SESS_VARS_SOURCED
+      source "/Users/jack.wright/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    '';
   };
 }
