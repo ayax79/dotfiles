bbe9fb5a9769b71a6c654bcec2bf93926321f6cd
Jack Wright <jack.wright@disqo.com>
2023-12-05 09:01:18 -0800

    Added Cargo.toml to project related files

--- home-manager/nvim/lua/plugins.lua
+++ home-manager/nvim/lua/plugins.lua
@@ -300,7 +300,7 @@ table.insert(M, {
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
-                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "gradlew" },
+                patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "gradlew", "Cargo.toml" },
             })
         end,
     },
