73f38ce9143a9ce57ac93b7c8b64d56d70862fe4
Jack Wright <jack.wright@disqo.com>
2023-12-14 16:27:50 -0800

    remove comments

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,30 +1,9 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{pkgs, ...}: 
-#let
-#   java-debug = pkgs.stdenv.mkDerivation {
-#     pname = "java-debug";
-#     version = "0.49.0";
-#     buildInputs = [pkgs.openjdk17 pkgs.maven];
-#     src = pkgs.fetchFromGitHub {
-#       owner = "microsoft";
-#       repo = "java-debug";
-#       rev = "refs/tags/0.49.0";
-#       hash = "sha256-i4dQNJtpXgX0d/wicWvEl25UO2sVCjOLDZAIDrV/gUw=";
-#     };
-#     buildPhase = ''
-#       mvn package -Dmaven.repo.local=$out/.m2
-#     '';
-#   };
-#in {
-{
+{pkgs, ...}: {
   home.packages = [
     pkgs.hadolint
     pkgs.dprint
     pkgs.yq
-    #pkgs.markdownlint-cli
     pkgs.tree-sitter
-    # pkgs.luarocks
     pkgs.nil # nix language server
     pkgs.jdt-language-server # java language server
     pkgs.nodePackages.vscode-langservers-extracted
@@ -35,7 +14,6 @@
     pkgs.alejandra # Alejandra nix formatting
     pkgs.lua-language-server # Lua language server
     pkgs.vscode-extensions.vscjava.vscode-java-test
-    # java-debug
   ];
 
   programs.neovim = {
