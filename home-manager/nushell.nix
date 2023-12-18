dcac1644c2575f220061c8a37359bd37286a7895
Jack Wright <jack.wright@disqo.com>
2023-12-18 11:41:45 -0800

    only compile with dataframe features, not extra

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -6,7 +6,7 @@
   programs.nushell = {
     enable = true;
     package = pkgs.nushell.override {
-      additionalFeatures = p: ["extra" "dataframe"];
+      additionalFeatures = p: ["dataframe"];
     };
     configFile.source = ./nushell/config.nu;
     envFile.source = ./nushell/env.nu;
