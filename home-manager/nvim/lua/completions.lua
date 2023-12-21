b40e8adfd3eb44236986258693691a8776763b63
Jack Wright <jack.wright@disqo.com>
2023-12-21 12:15:52 -0800

    remove comments

--- home-manager/nvim/lua/completions.lua
+++ home-manager/nvim/lua/completions.lua
@@ -5,33 +5,6 @@ require("copilot").setup({
     panel = { enabled = false },
 })
 
--- local tabnine = require("cmp_tabnine.config")
---
--- tabnine:setup({
---     max_lines = 1000,
---     max_num_results = 20,
---     sort = true,
---     run_on_every_keystroke = true,
---     snippet_placeholder = "..",
---     ignored_file_types = {
---         -- default is not to ignore
---         -- uncomment to ignore in lua:
---         -- lua = true
---     },
---     show_prediction_strength = false,
--- })
--- -- official tab nine
--- require("tabnine").setup({
---     disable_auto_comment = true,
---     -- accept_keymap = "<Tab>", -- don't conflict with cmp
---     accept_keymap = "<F9>", -- don't conflict with cmp
---     dismiss_keymap = "<C->",
---     debounce_ms = 800,
---     suggestion_color = { gui = "#808080", cterm = 244 },
---     exclude_filetypes = { "TelescopePrompt" },
---     log_file_path = nil, -- absolute path to Tabnine log file
--- })
---
 -- github copilot
 require("copilot_cmp").setup()
 
