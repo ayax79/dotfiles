0f39eadce086d78505744d88d2f8bf5f8118828b
Jack Wright <jack.wright@disqo.com>
2024-01-26 12:46:59 -0800

    commit out profile hack

--- home-manager/bash.nix
+++ home-manager/bash.nix
@@ -1,10 +1,10 @@
 {...}: {
   programs.bash = {
     enable = true;
-    profileExtra = ''
-      # hack to deal with https://github.com/nix-community/home-manager/issues/3100
-      unset __HM_SESS_VARS_SOURCED
-      . "/Users/jack.wright/.nix-profile/etc/profile.d/hm-session-vars.sh"
-    '';
+  #   profileExtra = ''
+  #     # hack to deal with https://github.com/nix-community/home-manager/issues/3100
+  #     unset __HM_SESS_VARS_SOURCED
+  #     . "/Users/jack.wright/.nix-profile/etc/profile.d/hm-session-vars.sh"
+  #   '';
   };
 }
