fc70b8318e2c416c3e3f6f1cc1a5db23d4e89ba8
Jack Wright <jack.wright@disqo.com>
2024-04-04 12:33:58 -0700

    "updates"

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -2,7 +2,7 @@
   programs.nushell = {
     enable = true;
     package = pkgs.nushell.override {
-      additionalFeatures = p: ["dataframe" "extra"];
+      additionalFeatures = p: ["dataframe"];
     };
     configFile.source = ./nushell/config.nu;
     envFile.source = ./nushell/env.nu;
