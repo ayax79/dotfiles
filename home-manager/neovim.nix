d7cb1f9de62763ca602c4460736b2702073cdac8
Jack Wright <jack.wright@disqo.com>
2024-05-16 11:21:47 -0700

    Added an override for lua language server to upgrade to the newest to fix build.. there is a PR but it hasn't been merged yet.

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -12,7 +12,17 @@
     terraform-lsp # terrform language server
     nodePackages.pyright # Pyright language server
     alejandra # Alejandra nix formatting
-    lua-language-server # Lua language server
+    # Lua language server
+    (lua-language-server.overrideAttrs (finalAttrs: previousAttrs: {
+      version = "3.9.1";
+      src = fetchFromGitHub {
+        owner = "luals";
+        repo = "lua-language-server";
+        rev = finalAttrs.version;
+        hash = "sha256-M4eTrs5Ue2+b40TPdW4LZEACGYCE/J9dQodEk9d+gpY=";
+        fetchSubmodules = true;
+      };
+    }))
     vscode-extensions.vscjava.vscode-java-test
     # Currently broken on the mac
     # vscode-extensions.vadimcn.vscode-lldb.adapter
