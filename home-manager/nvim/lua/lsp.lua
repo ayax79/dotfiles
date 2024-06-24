d7b6816eed1c6415a626ab7dfedc395756e62766
Jack Wright <jack.wright@disqo.com>
2024-06-24 10:58:38 -0700

    Fixing LSP issues

--- home-manager/nvim/lua/lsp.lua
+++ home-manager/nvim/lua/lsp.lua
@@ -5,15 +5,33 @@ M = {}
 local navic = require("nvim-navic")
 
 M.setup_codelens_refresh = function(client, bufnr)
-    if client.supports_method("textDocument/codeLens") then
-        -- refresh codelens on TextChanged and InsertLeave as well
-        vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
-            buffer = bufnr,
-            callback = vim.lsp.codelens.refresh,
-        })
-        -- trigger codelens refresh
-        vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+    local status_ok, codelens_supported = pcall(function()
+        return client.supports_method("textDocument/codeLens")
+    end)
+    if not status_ok or not codelens_supported then
+        return
     end
+    local group = "lsp_code_lens_refresh"
+    local cl_events = { "BufEnter", "InsertLeave" }
+    local ok, cl_autocmds = pcall(vim.api.nvim_get_autocmds, {
+        group = group,
+        buffer = bufnr,
+        event = cl_events,
+    })
+    if ok and #cl_autocmds > 0 then
+        return
+    end
+    local cb = function()
+        if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(bufnr) then
+            vim.lsp.codelens.refresh({ bufnr = bufnr })
+        end
+    end
+    vim.api.nvim_create_augroup(group, { clear = false })
+    vim.api.nvim_create_autocmd(cl_events, {
+        group = group,
+        buffer = bufnr,
+        callback = cb,
+    })
 end
 
 M.on_attach_common = function(client, bufnr)
