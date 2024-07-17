218c8c55404bbd56c16add6a5b7027a2cec3ecd4
Jack Wright <jack.wright@disqo.com>
2024-07-17 16:09:08 -0700

    move metals config to lsp.lua and use on_attach_common

--- home-manager/nvim/lua/lsp.lua
+++ home-manager/nvim/lua/lsp.lua
@@ -366,5 +366,42 @@ M.setup = function()
     lspconfig.gopls.setup({
         on_attach = M.on_attach_common,
     })
+
+    --------------------------------------------------------
+    --- METALS (scala) CONFIGURATION
+    --------------------------------------------------------
+    local metals_config = require("metals").bare_config()
+
+    -- Example of settings
+    metals_config.settings = {
+        showImplicitArguments = true,
+        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+    }
+
+    -- *READ THIS*
+    -- I *highly* recommend setting statusBarProvider to true, however if you do,
+    -- you *have* to have a setting to display this in your statusline or else
+    -- you'll not see any messages from metals. There is more info in the help
+    -- docs about this
+    metals_config.init_options.statusBarProvider = "off"
+
+    -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+    metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    metals_config.on_attach = function(client, bufnr)
+        require("metals").setup_dap()
+        M.on_attach_common(client, bufnr)
+    end
+
+    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "scala", "sbt", "java" },
+        callback = function()
+            require("metals").initialize_or_attach(metals_config)
+        end,
+        group = nvim_metals_group,
+    })
+
+
 end
 return M
