df8d4a43d9cd20bf44a3b6aec19df11c20e45112
Jack Wright <jack.wright@disqo.com>
2024-08-06 16:18:49 -0700

    comment out some unused stuff

--- home-manager/nvim/lua/plugins.lua
+++ home-manager/nvim/lua/plugins.lua
@@ -250,11 +250,11 @@ table.insert(M, {
             "nvim-telescope/telescope-ui-select.nvim",
         },
         config = function()
-            local actions = require("telescope.actions")
+            -- local actions = require("telescope.actions")
             local open_with_trouble = require("trouble.sources.telescope").open
 
             -- Use this to add more results without clearing the trouble list
-            local add_to_trouble = require("trouble.sources.telescope").add
+            -- local add_to_trouble = require("trouble.sources.telescope").add
 
             local telescope = require("telescope")
 
