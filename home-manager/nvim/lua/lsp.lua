81d17d4f837ce1bbc2f2cecf7c590a1d7da9952a
Jack Wright <jack.wright@disqo.com>
2024-09-07 16:57:08 -0700

    fix dap configuration

--- home-manager/nvim/lua/lsp.lua
+++ home-manager/nvim/lua/lsp.lua
@@ -116,11 +116,14 @@ M.setup = function()
 
     require("lsp-colors").setup()
 
+    local codelldb_home = vim.fn.stdpath('data')  .. '/mason/packages/codelldb/extension/'
+    local codelldb_path = codelldb_home .. 'adapter/codelldb'
+
     local dap, dapui = require("dap"), require("dapui")
     dapui.setup()
     dap.adapters.lldb = {
         type = "executable",
-        command = vim.fn.expand("~") .. "/.nix-profile/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb",
+        command = codelldb_path,
         name = "lldb",
     }
 
@@ -139,15 +142,13 @@ M.setup = function()
 
     vim.g.rustaceanvim = function()
         -- Update this path
-        local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/'
-        local codelldb_path = extension_path .. 'adapter/codelldb'
-        local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+        local liblldb_path = codelldb_home .. 'lldb/lib/liblldb'
         local this_os = vim.uv.os_uname().sysname;
 
         -- The path is different on Windows
         if this_os:find "Windows" then
-            codelldb_path = extension_path .. "adapter\\codelldb.exe"
-            liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+            codelldb_path = codelldb_home .. "adapter\\codelldb.exe"
+            liblldb_path = codelldb_home .. "lldb\\bin\\liblldb.dll"
         else
             -- The liblldb extension is .so for Linux and .dylib for MacOS
             liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
