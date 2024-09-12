d6efa1de7a832a26d0ce0ffec426b312095b1435
Jack Wright <jack.wright@disqo.com>
2024-09-12 10:47:30 -0700

    Theme work.
    Using onenord for alacritty and updated the zellij nord theme to use a
    different background color so I can actually see the cursor selection.

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -48,70 +48,126 @@
           startup_mode = "Maximized";
         };
 
-        # Nord theme colors
+        # one nord
         colors = {
           primary = {
-            # background = "#2e3440";
-            background = "#000000";
-            foreground = "#d8dee9";
-            dim_foreground = "#a5abb6";
+            background = "#2E3440";
+            foreground = "#C8D0E0";
           };
-
-          cursor = {
-            text = "#2e3440";
-            cursor = "#d8dee9";
+          normal = {
+            black = "#3B4252";
+            red = "#BF616A";
+            green = "#A3BE8C";
+            yellow = "#EBCB8B";
+            blue = "#81A1C1";
+            magenta = "#B988B0";
+            cyan = "#88C0D0";
+            white = "#E5E9F0";
           };
-
-          vi_mode_cursor = {
-            text = "#2e3440";
-            cursor = "#d8dee9";
+          bright = {
+            black = "#4C566A";
+            red = "#BF616A";
+            green = "#A3BE8C";
+            yellow = "#EBCB8B";
+            blue = "#81A1C1";
+            magenta = "#B988B0";
+            cyan = "#8FBCBB";
+            white = "#ECEFF4";
           };
-
-          selection = {
-            text = "CellForeground";
-            background = "#4c566a";
-          };
-
           search = {
             matches = {
-              foreground = "CellBackground";
-              background = "#88c0d0";
+              foreground = "#81A1C1";
+              background = "#4C566A";
+            };
+            focused_match = {
+              foreground = "#EBCB8B";
+              background = "#4C566A";
+            };
+            # footer_bar = {
+            #   background = "#434C5E";
+            #   foreground = "#88C0D0";
+            # };
+          };
+          hints = {
+            start = {
+              foreground = "#B988B0";
+              background = "#4C566A";
+            };
+            end = {
+              foreground = "#81A1C1";
+              background = "#4C566A";
             };
           };
-
-          normal = {
-            black = "#3b4252";
-            red = "#bf616a";
-            green = "#a3be8c";
-            yellow = "#ebcb8b";
-            blue = "#81a1c1";
-            magenta = "#b48ead";
-            cyan = "#88c0d0";
-            white = "#e5e9f0";
-          };
-
-          bright = {
-            black = "#4c566a";
-            red = "#bf616a";
-            green = "#a3be8c";
-            yellow = "#ebcb8b";
-            blue = "#81a1c1";
-            magenta = "#b48ead";
-            cyan = "#8fbcbb";
-            white = "#eceff4";
-          };
-
-          dim = {
-            black = "#373e4d";
-            red = "#94545d";
-            green = "#809575";
-            yellow = "#b29e75";
-            blue = "#68809a";
-            magenta = "#8c738c";
-            cyan = "#6d96a5";
-            white = "#aeb3bb";
+          selection = {
+            text = "CellForeground";
+            background = "#3F4758";
           };
         };
+
+        # Nord theme colors
+        # colors = {
+        #   primary = {
+        #     # background = "#2e3440";
+        #     background = "#000000";
+        #     foreground = "#d8dee9";
+        #     dim_foreground = "#a5abb6";
+        #   };
+        #
+        #   cursor = {
+        #     text = "#2e3440";
+        #     cursor = "#d8dee9";
+        #   };
+        #
+        #   vi_mode_cursor = {
+        #     text = "#2e3440";
+        #     cursor = "#d8dee9";
+        #   };
+        #
+        #   selection = {
+        #     text = "CellForeground";
+        #     background = "#4c566a";
+        #   };
+        #
+        #   search = {
+        #     matches = {
+        #       foreground = "CellBackground";
+        #       background = "#88c0d0";
+        #     };
+        #   };
+        #
+        #   normal = {
+        #     black = "#3b4252";
+        #     red = "#bf616a";
+        #     green = "#a3be8c";
+        #     yellow = "#ebcb8b";
+        #     blue = "#81a1c1";
+        #     magenta = "#b48ead";
+        #     cyan = "#88c0d0";
+        #     white = "#e5e9f0";
+        #   };
+        #
+        #   bright = {
+        #     black = "#4c566a";
+        #     red = "#bf616a";
+        #     green = "#a3be8c";
+        #     yellow = "#ebcb8b";
+        #     blue = "#81a1c1";
+        #     magenta = "#b48ead";
+        #     cyan = "#8fbcbb";
+        #     white = "#eceff4";
+        #   };
+        #
+        #   dim = {
+        #     black = "#373e4d";
+        #     red = "#94545d";
+        #     green = "#809575";
+        #     yellow = "#b29e75";
+        #     blue = "#68809a";
+        #     magenta = "#8c738c";
+        #     cyan = "#6d96a5";
+        #     white = "#aeb3bb";
+        #   };
+        # };
       };
     }
     (lib.optionalAttrs (lib.strings.hasInfix "darwin" pkgs.system) {
