97ebf21537f74d8ced55f69cad67bfd76dee7be2
Jack Wright <jack.wright@disqo.com>
2024-09-12 10:56:23 -0700

    zellij utils

--- home-manager/nushell.nix
+++ home-manager/nushell.nix
@@ -61,6 +61,7 @@
       use ~/.config/nushell/ext/file-utils.nu es
       use ~/.config/nushell/ext/neovim.nu *
       # use ~/.config/nushell/ext/tmux-utils.nu
+      use ~/.config/nushell/ext/zellij-utils.nu
     '';
   };
 
