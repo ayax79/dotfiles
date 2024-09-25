4c271d50a9ed597519988f5791bd3d9fc5492c8e
Jack Wright <jack.wright@disqo.com>
2024-09-25 11:19:47 -0700

    disable downloading of autolock plugin for zellij

--- home-manager/zellij/default.nix
+++ home-manager/zellij/default.nix
@@ -11,10 +11,10 @@
     source = ./zellij;
   };
 
-  home.file."${config.xdg.cacheHome}/zellij/zellij-autolock.wasm" = {
-    source = builtins.fetchurl {
-      url = "https://github.com/fresh2dev/zellij-autolock/releases/download/0.1.0/zellij-autolock.wasm";
-      sha256 = "19dsm3f0smvjicncm0zczzvv99853p27a3c4c9xrk6j8hhvmvaxb";
-    };
-  };
+  # home.file."${config.xdg.cacheHome}/zellij/zellij-autolock.wasm" = {
+  #   source = builtins.fetchurl {
+  #     url = "https://github.com/fresh2dev/zellij-autolock/releases/download/0.1.0/zellij-autolock.wasm";
+  #     sha256 = "19dsm3f0smvjicncm0zczzvv99853p27a3c4c9xrk6j8hhvmvaxb";
+  #   };
+  # };
 }
