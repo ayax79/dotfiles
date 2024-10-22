91203faa56042e0996d32bf5f62ec5150553f1a0
Jack Wright <jack.wright@disqo.com>
2024-10-21 17:22:29 -0700

    removed zellij plugin

--- home-manager/nvim/lua/plugins.lua
+++ home-manager/nvim/lua/plugins.lua
@@ -571,18 +571,6 @@ table.insert(M, {
     --     end
     -- },
     {
-        "https://git.sr.ht/~swaits/zellij-nav.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        keys = {
-            { "<c-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left" } },
-            { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down" } },
-            { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up" } },
-            { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
-        },
-        opts = {},
-    },
-    {
         "norcalli/nvim-colorizer.lua",
         lazy = false,
         config = function() require("colorizer").setup() end,
